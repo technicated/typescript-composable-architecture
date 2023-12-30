dd9b77b796f9d0f4500a20810fe59f90c5c06823
technicated <andrea.tomarelli+technicated@gmail.com>
2023-12-30 11:31:31 +0100

    Effect base functionalities

---
+++ src/lib/effect.spec.ts
@@ -0,0 +1,80 @@
+import test, { ExecutionContext } from 'ava'
+import { delay, of } from 'rxjs'
+import { TestScheduler } from 'rxjs/testing'
+import { Effect } from './effect'
+
+const makeTestScheduler = (t: ExecutionContext<unknown>) =>
+  new TestScheduler((actual, expected) => t.deepEqual(actual, expected))
+
+test('Effect.fireAndForget', (t) => {
+  const testScheduler = makeTestScheduler(t)
+
+  testScheduler.run(({ expectObservable }) => {
+    let executed = false
+    const e = Effect.fireAndForget(() => (executed = true))
+
+    t.false(executed)
+    t.truthy(e.source)
+
+    expectObservable(e.source!).toBe('|')
+    testScheduler.flush()
+
+    t.true(executed)
+  })
+})
+
+test('Effect.merge', (t) => {
+  const testScheduler = makeTestScheduler(t)
+
+  testScheduler.run(({ expectObservable }) => {
+    const e = Effect.merge(
+      Effect.observable(of(1)),
+      Effect.observable(of(2).pipe(delay(500))),
+      Effect.observable(of(3).pipe(delay(1000))),
+    )
+
+    t.truthy(e.source)
+
+    expectObservable(e.source!).toBe('a 499ms b 499ms (c|)', {
+      a: 1,
+      b: 2,
+      c: 3,
+    })
+  })
+})
+
+test('Effect.prototype.map', (t) => {
+  const testScheduler = makeTestScheduler(t)
+
+  testScheduler.run(({ expectObservable }) => {
+    const e1 = Effect.observable(of(42)).map((n) => `${n * n}`)
+    const e2 = Effect.none<number>().map((n) => `${n * n}`)
+    t.truthy(e1.source)
+    t.falsy(e2.source)
+    expectObservable(e1.source!).toBe('(a|)', { a: '1764' })
+  })
+})
+
+test('Effect.prototype.merge', (t) => {
+  const testScheduler = makeTestScheduler(t)
+
+  testScheduler.run(({ expectObservable }) => {
+    const base = Effect.observable(of(42))
+
+    const e1 = base.merge(Effect.none())
+    const e2 = Effect.none().merge(base)
+    const e3 = base.merge(Effect.observable(of(-1)))
+
+    t.truthy(e1.source)
+    t.truthy(e2.source)
+    t.truthy(e3.source)
+
+    t.true(e1 === base)
+    t.true(e2 === base)
+    t.true(e3 !== base)
+
+    expectObservable(e1.source!).toBe('(a|)', { a: 42 })
+    expectObservable(e2.source!).toBe('(a|)', { a: 42 })
+    expectObservable(e3.source!).toBe('(ab|)', { a: 42, b: -1 })
+  })
+})
--- src/lib/effect.ts
+++ src/lib/effect.ts
@@ -1,19 +1,73 @@
-import { map, Observable } from 'rxjs'
+import { defer, EMPTY, Observable, map as rxMap, merge as rxMerge } from 'rxjs'
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class Effect<Action> {
+  static fireAndForget<Action>(work: () => void): Effect<Action> {
+    return Effect.observable<Action>(
+      defer(() => {
+        work()
+        return EMPTY
+      }),
+    )
+  }
+
   static observable<Action>(source: Observable<Action>): Effect<Action> {
     return new Effect(source)
   }
 
-  static none(): Effect<never> {
+  static none<Action>(): Effect<Action> {
     return new Effect()
   }
 
   private constructor(public readonly source?: Observable<Action>) {}
+}
 
-  map<U>(transform: (action: Action) => U): Effect<U> {
-    return this.source
-      ? new Effect(this.source.pipe(map(transform)))
-      : new Effect()
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Effect {
+  interface MergeFn {
+    <Action>(...effects: Effect<Action>[]): Effect<Action>
+    <Action>(effects: Effect<Action>[]): Effect<Action>
   }
+
+  export const merge: MergeFn = function merge<Action>(
+    ...args: [Effect<Action>[]] | Effect<Action>[] | []
+  ): Effect<Action> {
+    const effects = (
+      Array.isArray(args[0]) ? args[0] : args
+    ) as Effect<Action>[]
+
+    return effects.reduce(
+      (effects, effect) => effects.merge(effect),
+      Effect.none(),
+    )
+  }
+}
+
+export interface Effect<Action> {
+  merge(other: Effect<Action>): Effect<Action>
+  map<U>(transform: (action: Action) => U): Effect<U>
+}
+
+Effect.prototype.merge = function merge<Action>(
+  this: Effect<Action>,
+  other: Effect<Action>,
+): Effect<Action> {
+  if (!other.source) {
+    return this
+  }
+
+  if (!this.source) {
+    return other
+  }
+
+  return Effect.observable(rxMerge(this.source, other.source))
+}
+
+Effect.prototype.map = function map<Action, U>(
+  this: Effect<Action>,
+  transform: (action: Action) => U,
+): Effect<U> {
+  return this.source
+    ? Effect.observable(this.source.pipe(rxMap(transform)))
+    : Effect.none()
 }
